0339de38c16e2a726e81abdda4a03a190f67b487
clubPenguin420 <42702641+clubPenguin420@users.noreply.github.com>
2022-03-31 13:51:10 -0500

    Fixing plotting(not fully fixed)

--- src/main/java/ImPlotExample.java
+++ src/main/java/ImPlotExample.java
@@ -7,7 +7,7 @@ import java.awt.Desktop;
 import java.net.URI;
 
 public class ImPlotExample {
-    static double lb = 0;
+    static double lb = -2*Math.PI;
     static double ub = 2*Math.PI;
     static int terms = 4096;
 
@@ -24,8 +24,8 @@ public class ImPlotExample {
     public static void show(ImBoolean showImPlotWindow) {
 //        generate(terms, lb, ub);
         convertData(FFT.generate(terms, lb, ub), xd, yd);
-//        convertData(FFT.fft(FFT.generate(terms, lb, ub)), xf, yf);
-        yf = formatFFTData(FFT.fft(FFT.generate(terms, lb, ub)));
+        convertData(FFT.fft(FFT.generate(terms, lb, ub)), xf, yf);
+//        yf = formatFFTData(FFT.fft(FFT.generate(terms, lb, ub)));
         ImGui.setNextWindowSize(900, 700, ImGuiCond.Once);
         ImGui.setNextWindowPos(ImGui.getMainViewport().getPosX(), ImGui.getMainViewport().getPosY(), ImGuiCond.Once);
         if (ImGui.begin("Fourier Analysis", showImPlotWindow)) {
@@ -59,10 +59,12 @@ public class ImPlotExample {
 
     public static void convertData(Complex[] cdata, Double[] x, Double[] y) {
         int i = 0;
+        double j = lb;
         for(Complex num : cdata) {
-            x[i] = (double)i;
+            x[i] = j;
             y[i] = num.re();
             ++i;
+            j += (ub - lb) / terms;
         }
     }
 
